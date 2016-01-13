76678f383c94e63ade9036ff28a4050c7b2a741e
Philip Doxakis <doxakisp@gmail.com>
2016-01-13 16:39:13 -0500

    Update Generate DbContext with SQL Server Database.sql

--- Generate DbContext with SQL Server Database.sql
+++ Generate DbContext with SQL Server Database.sql
@@ -22,21 +22,20 @@
 
 /*
 Script Name:
-    Auto generate context
+	Auto generate context
 Author:
 	Philip Doxakis
 Customize:
 	You can change the table suffix.
-    You can specify the context name. (by default it will use the database name)
+	You can specify the context name. (by default it will use the database name)
 Description:
 	Generate the DbContext class for entity framework.
-    It will generate navigation properties based on foreign key.
+	It will generate navigation properties based on foreign key.
 Limitations:
 	- Table primary key are: TableName + Id or Id
-    - Foreign key column end with Id or ID
-    - Many to 1 are the only supported relationship.
-    (1 to 1 can be simulated with Many to 1)
-    - Most data type are supported.
+	- Foreign key column end with Id or ID
+	- Many to 1 are the only supported relationship. (1 to 1 can be simulated with Many to 1)
+	- Most data type are supported.
 */
 
 -- Options:
